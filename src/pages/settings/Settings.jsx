f118ccac67ade28fac7042567efd84ac29c3c5ed
Saksham <gsaksham2000@gmail.com>
2022-01-25 15:01:36 +0530

    Added MAterial UI comps in Settings

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -2,6 +2,19 @@ import "./settings.css"
 import Sidebar from "./../../components/sidebar/Sidebar";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import IconButton from '@mui/material/IconButton';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+
+const buttonTheme = createTheme({
+    palette: {
+        primary: {
+            main: "#000000",
+        },
+    },
+});
+
 
 export default function Settings() {
     return (
@@ -24,15 +37,31 @@ export default function Settings() {
                         <input type="file" id="fileInput" style={{ display: "none" }} />
 
                     </div>
-                    <label>Username</label>
-                    <input type="text" placeholder="Safak" name="name" />
-                    <label>Email</label>
+                    {/* <label>Username</label>
+                    <input type="text" placeholder="Safak" name="name" /> */}
+                    <div className="settingsTF">
+                        <TextField id="filled-basic" label="Username" variant="filled" type={"text"} fullWidth/>
+
+                    </div>
+                    <div className="settingsTF">
+                        <TextField id="filled-basic" label="E-Mail" variant="filled" type={"email"} fullWidth/>
+
+                    </div>
+                    <div className="settingsTF">
+                        <TextField id="filled-basic" label="Password" variant="filled" type={"password"} fullWidth />
+
+                    </div>
+                    {/* <label>Email</label>
                     <input type="email" placeholder="safak@gmail.com" name="email" />
                     <label>Password</label>
-                    <input type="password" placeholder="Password" name="password" />
-                    <button className="settingsSubmitButton" type="submit">
-                        Update
-                    </button>
+                    <input type="password" placeholder="Password" name="password" /> */}
+                    <div className="updateButton">
+                        <ThemeProvider theme={buttonTheme}>
+                            <Button
+                                variant="outlined"
+                            >Update</Button>
+                        </ThemeProvider>
+                    </div>
                 </form>
             </div>
             <Sidebar />
