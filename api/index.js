74bf56aec84ae16e0fcf2afc8bca9914c8c704e6
Saksham <gsaksham2000@gmail.com>
2022-02-11 13:59:51 +0530

    Balancing git

--- api/index.js
+++ api/index.js
@@ -6,21 +6,19 @@ dotenv.config();
 
 const mongoose = require("mongoose");
 const PORT=process.env.PORT || 5000;
-const authRoute = require("./routes/auth.js");
+const authRoute=require("./routes/auth");
 
 app.use(express.json())
 
 mongoose.connect('mongodb://localhost:27017/blog', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify:true
+    useUnifiedTopology: true
   })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
 
-app.use("api/auth",authRoute);
+app.use("/api/auth",authRoute);
 
 app.listen(PORT,()=>{
     console.log(`server is running on PORT ${PORT}`)
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,18 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 //REGISTER
 
 router.get('/', function (req, res) {
     res.send('hello world')
-  })
+})
 
 router.post("/register", async (req, res) => {
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hashedPass,
     });
 
     const user = await newUser.save();
