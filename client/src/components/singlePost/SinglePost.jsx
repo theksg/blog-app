7b01c9e15869dada355fe070ac93b8054ff16342
Saksham <gsaksham2000@gmail.com>
2022-06-18 10:17:55 +0530

    Changed Indentation of SinglePost for bettter readability

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -34,41 +34,44 @@ export default function SinglePost() {
         } catch (err) {}
       };
       if(updateMode){
-          post.update=true;
-      return <Write post={post}/>}
+        post.update=true;
+        return <Write post={post}/>
+      }
       else
-  return (
-  <div className="singlePost">
-      <div className="singlePostWrapper">
-          {post.photo &&(
+        return (
+        <div className="singlePost">
+            <div className="singlePostWrapper">
+                {
+                post.photo &&(
+                <img src={post.photo} 
+                alt="" className="singlePostImg" />
+                )
+                }
+                <h1 className="singlePostTitle">
+                    {post.title}
+                    {
+                        post.username === user?.username && (
+                            <div className="singlePostEdit">
+                                <i className="singlePostIcon far fa-edit" onClick={()=> setUpdateMode(true)}></i>
+                                <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+                            </div>
+                        )
+                    }
+                    
+                </h1>
 
-          <img src={post.photo} 
-          alt="" className="singlePostImg" />
-          )}
-          <h1 className="singlePostTitle">
-              {post.title}
-              {
-                  post.username === user?.username && (
-                    <div className="singlePostEdit">
-                        <i className="singlePostIcon far fa-edit" onClick={()=> setUpdateMode(true)}></i>
-                        <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
-                    </div>
-                  )
-              }
-              
-          </h1>
-
-          <div className="singlePostInfo">
-              <span className="singlePostAuthor">Author:
-              <Link to={`/?username=${post.username}`} className='link'>
-                <b> {post.username}</b>
-              </Link>
-              </span>
-              <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
-          </div>
-          <p className="singlePostDesc">
-              {post.desc}
-          </p>
-      </div>
-  </div>);
+                <div className="singlePostInfo">
+                    <span className="singlePostAuthor">Author:
+                    <Link to={`/?username=${post.username}`} className='link'>
+                        <b> {post.username}</b>
+                    </Link>
+                    </span>
+                    <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
+                </div>
+                <p className="singlePostDesc">
+                    {post.desc}
+                </p>
+            </div>
+        </div>
+        );
 }
