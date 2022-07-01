6a7fb227cdf1b0f0553ac600cc5dd57f36184656
Saksham <gsaksham2000@gmail.com>
2022-07-01 15:36:16 +0530

    Added Search Bar and enable it to search post category

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./home.css";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
@@ -25,6 +26,9 @@ const Home = () =>{
         <>
         <Header/>
         <a id="posts"></a>
+        <div className="searchBar">
+            <SearchBar/>
+        </div>
         <div className="home">
             <Posts posts={posts}/>
         </div>
